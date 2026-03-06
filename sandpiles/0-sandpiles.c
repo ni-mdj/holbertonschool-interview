9017964a9abb1a68c22da883b780eaaaf0eca45d
TonNom <ton-email@example.com>
2026-03-06 14:24:32 +0100

    add

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -2,30 +2,9 @@
 #include "sandpiles.h"
 
 /**
-* is_stable - Check if a 3x3 grid is stable
-* @grid: 3x3 grid
-*
-* Return: 1 if stable, 0 otherwise
-*/
-static int is_stable(int grid[3][3])
-{
-	int i, j;
-
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			if (grid[i][j] > 3)
-				return (0);
-		}
-	}
-	return (1);
-}
-
-/**
-* print_grid - Print a 3x3 grid
-* @grid: 3x3 grid
-*/
+ * print_grid - Print 3x3 grid
+ * @grid: 3x3 grid
+ */
 static void print_grid(int grid[3][3])
 {
 	int i, j;
@@ -43,46 +22,68 @@ static void print_grid(int grid[3][3])
 }
 
 /**
-* topple - Perform one toppling round
-* @grid: 3x3 grid
-*/
-static void topple(int grid[3][3])
+ * is_stable - Check if a sandpile is stable
+ * @grid: 3x3 grid to check
+ *
+ * Return: 1 if stable, 0 if unstable
+ */
+static int is_stable(int grid[3][3])
 {
 	int i, j;
-
-	int tmp[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
 		{
 			if (grid[i][j] > 3)
-			{
-				tmp[i][j] -= 4;
-				if (i > 0)
-					tmp[i - 1][j] += 1;
-				if (i < 2)
-					tmp[i + 1][j] += 1;
-				if (j > 0)
-					tmp[i][j - 1] += 1;
-				if (j < 2)
-					tmp[i][j + 1] += 1;
-			}
+				return (0);
+		}
+	}
+	return (1);
+}
+
+/**
+ * topple - Perform one toppling round on unstable sandpile
+ * @grid: 3x3 grid to topple
+ */
+static void topple(int grid[3][3])
+{
+	int i, j;
+	int temp[3][3];
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			temp[i][j] = grid[i][j];
 		}
 	}
 
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
-			grid[i][j] += tmp[i][j];
+		{
+			if (temp[i][j] > 3)
+			{
+				grid[i][j] -= 4;
+				if (i > 0)
+					grid[i - 1][j] += 1;
+				if (i < 2)
+					grid[i + 1][j] += 1;
+				if (j > 0)
+					grid[i][j - 1] += 1;
+				if (j < 2)
+					grid[i][j + 1] += 1;
+			}
+		}
 	}
 }
 
 /**
-* sandpiles_sum - Compute the sum of two sandpiles
-* @grid1: Left 3x3 grid
-* @grid2: Right 3x3 grid
-*/
+ * sandpiles_sum - Computes the sum of two sandpiles
+ * @grid1: First sandpile (result will be stored here)
+ * @grid2: Second sandpile
+ */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
 	int i, j;
@@ -90,16 +91,15 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
+		{
 			grid1[i][j] += grid2[i][j];
+		}
 	}
 
 	while (!is_stable(grid1))
 	{
+		printf("=\n");
+		print_grid(grid1);
 		topple(grid1);
-		if (!is_stable(grid1))
-		{
-			printf("=\n");
-			print_grid(grid1);
-		}
 	}
 }
