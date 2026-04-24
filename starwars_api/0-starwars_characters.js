d45661414856590c8d11e2ba691265256abcb6c9
TonNom <ton-email@example.com>
2026-04-24 14:56:17 +0200

    flag of task problem with connexion ghitub

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -2,61 +2,48 @@
 
 const request = require('request');
 
-if (process.argv.length < 3) {
-	console.error('Usage: ./0-starwars_characters.js <Movie ID>');
-	process.exit(1);
+const movieId = process.argv[2];
+
+if (!movieId) {
+  console.error('Usage: ./0-starwars_characters.js <movie_id>');
+  process.exit(1);
 }
 
-const movieId = process.argv[2];
-const filmUrl = `https://swapi-api.alholbertondevops.com/api/films/${movieId}/`;
+const filmUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
 request(filmUrl, (error, response, body) => {
-	if (error) {
-		console.error(error);
-		process.exit(1);
-	}
+  if (error) {
+    console.error(error);
+    return;
+  }
 
-	if (response.statusCode !== 200) {
-		console.error(`Error: Status code ${response.statusCode}`);
-		process.exit(1);
-	}
+  if (response.statusCode !== 200) {
+    console.error(`Failed to fetch movie: ${response.statusCode}`);
+    return;
+  }
 
-	try {
-		const film = JSON.parse(body);
-		const characters = film.characters;
+  const filmData = JSON.parse(body);
+  const characters = filmData.characters;
 
-		if (!characters || characters.length === 0) {
-			process.exit(0);
-		}
-
-		let index = 0;
-
-		const printCharacter = () => {
-			if (index >= characters.length) {
-				process.exit(0);
-			}
-
-			request(characters[index], (error, response, body) => {
-				if (error) {
-					console.error(error);
-					process.exit(1);
-				}
-
-				try {
-					const character = JSON.parse(body);
-					console.log(character.name);
-					index++;
-					printCharacter();
-				} catch (e) {
-					console.error(e);
-					process.exit(1);
-				}
-			});
-		};
-
-		printCharacter();
-	} catch (e) {
-		console.error(e);
-		process.exit(1);
-	}
+  printCharacters(characters, 0);
 });
+
+function printCharacters (characterUrls, index) {
+  if (index >= characterUrls.length) {
+    return;
+  }
+
+  request(characterUrls[index], (error, response, body) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    if (response.statusCode === 200) {
+      const characterData = JSON.parse(body);
+      console.log(characterData.name);
+    }
+
+    printCharacters(characterUrls, index + 1);
+  });
+}
